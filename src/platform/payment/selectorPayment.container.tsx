b2ff84095dac4fac8141cd188e6852790dedcc97
AlvaroCV1995 <30363055+AlvaroAlonsoCajusolVallejos@users.noreply.github.com>
2022-06-06 01:59:11 -0500

    fix save in localstorage

--- src/platform/payment/selectorPayment.container.tsx
+++ src/platform/payment/selectorPayment.container.tsx
@@ -1,27 +1,16 @@
 import React from 'react';
-import useStudentOrders, { StudentOrdersState } from '../../shared/hooks/studentOrders.hook';
+import { StudentOrdersState } from '../../shared/hooks/studentOrders.hook';
 import { StudentInfo } from '../../shared/models/StudentInfo';
 import DisplayTemplateContext from '../../shared/templates/DisplayNew/contexts/displayTemplate.context';
 import SelectorPaymentViewProps from './selectorPayment.view';
 
 const SelectorPaymentContainer = (): React.ReactElement => {
-  const studentOrders = useStudentOrders();
-  if (
-    !localStorage.getItem('selectedStudentOrders') &&
-    studentOrders.isLoading &&
-    studentOrders.data.length
-  ) {
-    localStorage.setItem('selectedStudentOrders', JSON.stringify(studentOrders));
-  }
-  const [selectedStudentOrders, setSelectedStudentOrders] = React.useState<StudentOrdersState>(
-    localStorage.getItem('selectedStudentOrders')
-      ? JSON.parse(localStorage.getItem('selectedStudentOrders')!)
-      : studentOrders,
-  );
   const { setToolbarTitle } = React.useContext(DisplayTemplateContext);
   const student: StudentInfo = JSON.parse(localStorage.getItem('selectedStudent')!);
-
   if (student && setToolbarTitle) setToolbarTitle(student.school.name);
+  const [selectedStudentOrders, setSelectedStudentOrders] = React.useState<StudentOrdersState>(
+    JSON.parse(localStorage.getItem('selectedStudentOrders')!),
+  );
 
   return (
     <SelectorPaymentViewProps
--- src/platform/students/selectorStudents.container.tsx
+++ src/platform/students/selectorStudents.container.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
 import useStudentInfo from '../../shared/hooks/studentInfo.hook';
+import useStudentOrders from '../../shared/hooks/studentOrders.hook';
 import { StudentInfo } from '../../shared/models/StudentInfo';
 import SelectorStudentsView from './selectorStudents.view';
 
 const SelectorStudentsContainer = (): React.ReactElement => {
   const studentInfo = useStudentInfo();
+  const studentOrders = useStudentOrders();
   const navigate = useNavigate();
 
   const handleSelectedStudent = (student: StudentInfo) => {
     localStorage.setItem('selectedStudent', JSON.stringify(student));
-    localStorage.removeItem('selectedStudentOrders');
+    localStorage.setItem('selectedStudentOrders', JSON.stringify(studentOrders));
     navigate(`/payment/${student.last_name + student.first_name}`);
   };
 
